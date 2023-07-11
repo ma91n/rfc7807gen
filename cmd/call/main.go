a285f7bfa5e0f6412d9a9a2d7a04e9dbe7817c55
laqiiz <mano.junki@gmail.com>
2023-07-11 17:16:45 +0900

    1st commit

---
+++ cmd/call/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/ma91n/rfc7807gen/client"
+	"github.com/ma91n/rfc7807gen/client/example"
+)
+
+func main() {
+
+	rt := httptransport.New(
+		"localhost:60270",
+		client.DefaultBasePath,
+		client.DefaultSchemes)
+	rt.Consumers["application/problem+json"] = runtime.JSONConsumer()
+
+	exampleClient := client.New(rt, nil)
+
+	resp, err := exampleClient.Example.Hello((&example.HelloParams{}).WithContext(context.Background()))
+	if err != nil {
+		fmt.Println("!!!", err)
+		return
+	}
+	payload := resp.GetPayload()
+	fmt.Println(payload.Message)
+
+}
